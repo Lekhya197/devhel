b923837ad8fc632ccc6cd28fb0fa84903b700f4e
Lekhya197 <lekhya198@gmail.com>
2025-06-04 23:46:56 +0530

    app.py

--- app.py
+++ app.py
@@ -96,9 +96,12 @@ def process_frame():
 
     return jsonify({'detections': detections})
 
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # For Render
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
